aidilaryanto/ProjectDils

figlet: improve
Signed-off-by: aidilaryanto <aidilaryanto18@gmail.com>

userbot/modules/figlet.py
@@ -1,38 +1,43 @@
-import asyncio
+# Copyright (C) 2019 The Raphielscape Company LLC.
+#
+# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# you may not use this file except in compliance with the License.
+#
+ 
 import pyfiglet
-from telethon import events, functions
 from userbot import CMD_HELP
 from userbot.events import register
-import sys
  
-@register(outgoing=True, pattern="^.fg(?: |$)(.*)")
-async def figlet(event):
-    if event.fwd_from:
+@register(outgoing=True, pattern="^\.fg(?: |$)(.*)")
+async def figlet(e):
+    if e.fwd_from:
         return
     CMD_FIG = {"slant": "slant", "3D": "3-d", "5line": "5lineoblique", "alpha": "alphabet", "banner": "banner3-D", "doh": "doh", "iso": "isometric1", "letter": "letters", "allig": "alligator", "dotm": "dotmatrix", "bubble": "bubble", "bulb": "bulbhead", "digi": "digital"}
-    input_str = event.pattern_match.group(1)
-    if "|" in input_str:
-        text, cmd = input_str.split("|", maxsplit=1)
+    input_str = e.pattern_match.group(1)
+    if "." in input_str:
+        text, cmd = input_str.split(".", maxsplit=1)
     elif input_str is not None:
         cmd = None
         text = input_str
     else:
-        await event.edit("Please add some text to figlet")
+        await e.edit("`Please add some text to figlet`")
         return
     if cmd is not None:
         try:
             font = CMD_FIG[cmd]
         except KeyError:
-            await event.edit("Invalid selected font.")
+            await e.edit("`Invalid selected font.`")
             return
         result = pyfiglet.figlet_format(text, font=font)
     else:
         result = pyfiglet.figlet_format(text)
-    await event.respond("‌‌‎`{}`".format(result))
-    await event.delete()
-
+    await e.respond("‌‌‎`{}`".format(result))
+    await e.delete()
+ 
 CMD_HELP.update({
         "figlet":
-        "`>.fg`"
+        ">`.fg <text>.<style>`"
         "\nUsage: Enhance ur text to strip line with anvil."
+        "\n\nExample: `.fg USERBOT.3D`"
+        "\n\nStyle: `slant`, `3D`, `5line`, `alpha`, `banner`, `doh`, `iso`, `letter`, `allig`, `dotm`, `bubble`, `bulb`, `digi`"
 })

Deadpool61/Ubot

Signed-off-by: Heinz Der Flugel <heinzdf@outlook.com>
Signed-off-by: MoveAngel <moveangel@minaproject.tech>

userbot/modules/figlet.py
@@ -4,42 +4,42 @@
 # you may not use this file except in compliance with the License.
 #
 # Port to UserBot by @MoveAngel
-
-import asyncio
+ 
 import pyfiglet
-from telethon import events, functions
 from userbot import CMD_HELP
 from userbot.events import register
-import sys
- 
-@register(outgoing=True, pattern="^.figlet(?: |$)(.*)")
-async def figlet(event):
-    if event.fwd_from:
+
+@register(outgoing=True, pattern="^\.figlet(?: |$)(.*)")
+async def figlet(fg):
+    if fg.fwd_from:
         return
     CMD_FIG = {"slant": "slant", "3D": "3-d", "5line": "5lineoblique", "alpha": "alphabet", "banner": "banner3-D", "doh": "doh", "iso": "isometric1", "letter": "letters", "allig": "alligator", "dotm": "dotmatrix", "bubble": "bubble", "bulb": "bulbhead", "digi": "digital"}
-    input_str = event.pattern_match.group(1)
-    if "|" in input_str:
-        text, cmd = input_str.split("|", maxsplit=1)
+    input_str = fg.pattern_match.group(1)
+    if "." in input_str:
+        text, cmd = input_str.split(".", maxsplit=1)
     elif input_str is not None:
         cmd = None
         text = input_str
     else:
-        await event.edit("Please add some text to figlet")
+        await fg.edit("`Please add some text to figlet`")
         return
     if cmd is not None:
         try:
             font = CMD_FIG[cmd]
         except KeyError:
-            await event.edit("Invalid selected font.")
+            await fg.edit("`Invalid selected font.`")
             return
         result = pyfiglet.figlet_format(text, font=font)
     else:
         result = pyfiglet.figlet_format(text)
-    await event.respond("‌‌‎`{}`".format(result))
-    await event.delete()
-
+    await fg.respond("‌‌‎`{}`".format(result))
+    await fg.delete()
+ 
 CMD_HELP.update({
-        "figlet": 
-        ".figlet \
-          \nUsage: Enhance ur text to strip line with anvil.\n"
+        "figlet":
+        ".figlet"
+          "\nUsage: Enhance ur text to strip line with anvil."
+          "\n\nExample: `.figlet <Text Style>`"
+          "\nSTYLE LIST: `slant`, `3D`, `5line`, `alpha`, `banner`, `doh`, `iso`, `letter`, `allig`, `dotm`, `bubble`, `bulb`, `digi`"
     })
+ 